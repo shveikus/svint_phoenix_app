b687e669f662bb4277cc1493ddae592a6b9ccec1
svintbel <svintbel@gmail.com>
2019-10-26 23:34:54 +0300

    Add configs for deploying on heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :svint_phoenix_app, SvintPhoenixAppWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "svint-phoeinx", port: 443],
+force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
