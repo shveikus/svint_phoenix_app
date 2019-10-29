7c893f2957585fd23c84a09350e3bc8574ee03f9
svintbel <svintbel@gmail.com>
2019-10-29 12:44:59 +0300

    rewrite sign in buttons into login

--- lib/svint_phoenix_app_web.ex
+++ lib/svint_phoenix_app_web.ex
@@ -42,7 +42,6 @@ defmodule SvintPhoenixAppWeb do
       import SvintPhoenixAppWeb.ErrorHelpers
       import SvintPhoenixAppWeb.Gettext
       alias SvintPhoenixAppWeb.Router.Helpers, as: Routes
-      import SvintPhoenixAppWeb.Helpers.Auth
     end
   end
 
--- lib/svint_phoenix_app_web/helpers/auth.ex
+++
@@ -1,8 +0,0 @@
-defmodule SvintPhoenixAppWeb.Helpers.Auth do
-
-  def signed_in?(conn) do
-    user_id = Plug.Conn.get_session(conn, :current_user_id)
-    if user_id, do: !!SvintPhoenixApp.Repo.get(SvintPhoenixApp.Accounts.User, user_id)
-  end
-
-end
